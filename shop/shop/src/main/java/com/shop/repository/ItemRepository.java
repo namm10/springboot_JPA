b68e59f85db2447b2dbacd44a1e114351a38cdda
yooseyoung <hou7911@naver.com>
2023-04-06 16:50:37 +0900

    230406@Query어노테이션이용

--- shop/shop/src/main/java/com/shop/repository/ItemRepository.java
+++ shop/shop/src/main/java/com/shop/repository/ItemRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 //JpaRepository를 상속받는 ItemRepository를 작성
 //Item클래스는 기본키 타입이 Long이므로 Long을 넣어줌
 public interface ItemRepository extends JpaRepository<Item, Long> {
@@ -25,4 +28,20 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     //내림차순
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    //@Query 어노테이션 안에 JPQL로 작성한 쿼리문을 넣어줍니다.
+    //from 뒤에는 엔티티 클래스로 작성한 Item을 지정해주었고, Item으로부터 데이터를 select하겠다는 것을 의미
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    //파라미터에 @Param 어노테이션을 이용하여 파라미터로 넘어온 값을 JPQL에 들어갈 변수로 지정해줄 수 있습니다.
+    //현재는 itemDetail 변수를 "like % %" 사이에 "itemDetail"로 값이 들어가도록 작성했습니다.
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    //value 안에 네이티브 쿼리문을 작성하고 "nativeQuery=true"를 지정합니다.
+    //만약 기존의 데이터베이스에서 사용하던 쿼리를 그대로 사용하애 할 때는 @Query의 nativeQuery속성을 사용하면
+    //기존 쿼리를 그대로 활용할 수 있다 하지만 독립적이라는 장점을 잃어버린다.
+    //기존에 작성한 통계용 쿼리처럼 복잡한 쿼리를 그대로 사용해야 하는 경우 활용할 수 있다.
+    @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc",
+                    nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+
 }
